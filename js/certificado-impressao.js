7d94318925bbf8b04986a5b76a05549b1d49387d
vidasrenovadasassembleia-debug <vidasrenovadasassembleia@gmail.com>
2026-07-22 11:58:11 -0300

    Update certificado-impressao.js

--- js/certificado-impressao.js
+++ js/certificado-impressao.js
@@ -58,11 +58,9 @@ function preencherEstruturaCertificado(folha, dados = {}) {
 
   const nome = campo("nome");
 nome.textContent = dados.nome || "";
-ajustarTexto(nome, 32, 20);
 
 const cargo = campo("cargo");
 cargo.textContent = dados.cargo || "";
-ajustarTexto(cargo, 24, 15);
 
   const local = dados.cidade || dados.local || "";
   const data = dados.dataExtenso || "";
