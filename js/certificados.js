7d5a1d27d92a7ea70b70401affa98ce7075c0d70
vidasrenovadasassembleia-debug <vidasrenovadasassembleia@gmail.com>
2026-07-28 16:19:34 -0300

    Update certificados.js

--- js/certificados.js
+++ js/certificados.js
@@ -57,9 +57,9 @@ async function iniciarModuloCertificados() {
       : null;
 
   if (!sessao || !sessao.credential) {
-  window.location.replace("index.html");
-  return;
-}
+    window.location.replace("index.html");
+    return;
+  }
 
   if (typeof aplicarIdentidadeUsuario === "function") {
     aplicarIdentidadeUsuario();
@@ -84,13 +84,13 @@ function configurarEventos() {
     });
   });
 
- $("#botaoSair")?.addEventListener("click", () => {
-  if (typeof CHAVE_SESSAO !== "undefined") {
-    sessionStorage.removeItem(CHAVE_SESSAO);
-  }
+  $("#botaoSair")?.addEventListener("click", () => {
+    if (typeof CHAVE_SESSAO !== "undefined") {
+      sessionStorage.removeItem(CHAVE_SESSAO);
+    }
 
-  window.location.href = "index.html";
-});
+    window.location.href = "index.html";
+  });
 
   Object.values(CONFIGURACAO_TIPOS_CERTIFICADO).forEach((configuracao) => {
     configurarPesquisaMembro(configuracao);
@@ -778,7 +778,7 @@ function montarCertificado(dados) {
   const caminhoLogo =
     arquivos.logo ||
     arquivos.logoIgreja ||
-    "../logo.png";
+    "logo.png";
 
   const caminhoAssinatura =
     arquivos.assinatura ||
