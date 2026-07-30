d2534903b53131dc86eae86ea329e3c763f98f1e
vidasrenovadasassembleia-debug <vidasrenovadasassembleia@gmail.com>
2026-07-30 08:54:17 -0300

    Update certificados.js

--- js/certificados.js
+++ js/certificados.js
@@ -27,7 +27,7 @@ const MODELOS_CERTIFICADO = Object.freeze({
       ano: { top: 735, left: 1080, width: 92, height: 32 },
       assinatura: { top: 803, left: 490, width: 500, height: 96 },
       registro: { top: 931, left: 350, width: 255, height: 32 },
-      qr: { top: 640, left: 1243, width: 140, height: 140 }
+      qr: { top: 640, left: 1251, width: 148, height: 148 }
     }
   },
 
