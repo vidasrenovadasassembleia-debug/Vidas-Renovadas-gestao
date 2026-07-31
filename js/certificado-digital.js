3cfb4a9cbcffbb24e9459f955e9e1b010eae7959
vidasrenovadasassembleia-debug <vidasrenovadasassembleia@gmail.com>
2026-07-31 14:02:18 -0300

    Update certificado-digital.js

--- js/certificado-digital.js
+++ js/certificado-digital.js
@@ -323,14 +323,19 @@ function gerarQr(dados) {
 
   url.searchParams.set("token", token);
 
-  new QRCode(alvo, {
-    text: url.href,
-    width: Math.max(96, alvo.clientWidth - 8),
-    height: Math.max(96, alvo.clientHeight - 8),
-    colorDark: "#071f3b",
-    colorLight: "#ffffff",
-    correctLevel: QRCode.CorrectLevel.M
-  });
+  const tamanho = Math.max(
+  96,
+  Math.min(alvo.clientWidth, alvo.clientHeight) - 8
+);
+
+new QRCode(alvo, {
+  text: url.href,
+  width: tamanho,
+  height: tamanho,
+  colorDark: "#071f3b",
+  colorLight: "#ffffff",
+  correctLevel: QRCode.CorrectLevel.M
+});
 }
 
 function separarData(valor) {
