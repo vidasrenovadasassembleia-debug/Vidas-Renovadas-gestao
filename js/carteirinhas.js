cb840b5aaf49aca4c02eedc698f29743d662247b
vidasrenovadasassembleia-debug <vidasrenovadasassembleia@gmail.com>
2026-08-12 09:06:20 -0300

    Update carteirinhas.js

--- js/carteirinhas.js
+++ js/carteirinhas.js
@@ -409,8 +409,8 @@
 
     new window.QRCode(elementos.qrCode, {
       text: urlPublica,
-      width: 135,
-      height: 135,
+      width: 174,
+      height: 174,
       correctLevel: window.QRCode.CorrectLevel.M
     });
 
